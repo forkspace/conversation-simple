5aa317ad9b49f5b9dbef3c02102c27ccbb38d3bf
jtviegas <jtviegas.js@google.com>
2017-11-08 23:43:09 +0000

    ...

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+workspaceId=18adb3ee-eb32-4e55-925c-1d0711f91227
+
+CONVERSATION_USERNAME=45991fa6-0aa7-4fd5-aaa2-1a1442ff6b84 CONVERSATION_PASSWORD=TWSbyxwcGdWj WORKSPACE_ID=$workspaceId node-debug server.js
+
